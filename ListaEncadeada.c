2b45161e44a3802d24cef4b9db9d0c45568ec96d
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-11-28 11:21:03 -0300

    Add files via upload

--- ListaEncadeada.c
+++ ListaEncadeada.c
@@ -4,12 +4,12 @@
 
 typedef struct Pessoa{
     int id;
-    char nome[30];
+    char nome[40];
     int idade;
     struct Pessoa *prox;
 }Pessoa;
 
-//criar lisra vazia
+//criar lista vazia
 Pessoa* criarListaVazia(){
     return NULL;
 }
@@ -28,7 +28,8 @@ Pessoa* cadastrar(Pessoa *lista){
     Pessoa *novaPessoa = criarPessoa();
     //inserir as informacoes
     printf("\nDigite o nome: ");
-    fgets(novaPessoa->nome, sizeof(novaPessoa), stdin);
+    fgets(novaPessoa->nome, sizeof(novaPessoa->nome), stdin);
+    fflush(stdin);
 
     printf("\nDigite a idade: ");
     scanf("%d", &novaPessoa->idade);
@@ -50,10 +51,97 @@ Pessoa* cadastrar(Pessoa *lista){
     }
 }
 
+void mostrar(Pessoa *lista){
+    if(lista == NULL){
+        printf("\n");
+        printf("Lista NULA");
+        printf("\n");
+        return;
+    } else {
+        Pessoa *atual = lista;
+        while (atual != NULL){
+            printf("\nNome: %s", atual->nome);
+            printf("Idade: %d", atual->idade);
+            printf("\nID: %d", atual->id);
+            printf("\n");
+            atual = atual->prox;
+        }
+    }
+}
+
+Pessoa* buscarPessoa(Pessoa *lista, int idBusca){
+    if(lista == NULL){
+        printf("\n");
+        printf("Lista nula");
+        printf("\n");
+        return NULL;
+    } else {
+        Pessoa *atual = lista;
+        while(atual != NULL){
+            if(atual->id == idBusca){
+            printf("\nNome: %s", atual->nome);
+            printf("Idade: %d", atual->idade);
+            printf("\nID: %d", atual->id);
+            printf("\n");
+            return atual;
+            }
+            atual = atual->prox;
+        }
+        printf("\nPessoa nao encontrada");
+        return NULL;
+    }
+}
+void alterar(Pessoa *encontrada){
+    printf("\nAlteracao dos campos");
+    fflush(stdin);
+    printf("\nDigite o nome: ");
+    fgets(encontrada->nome, sizeof(encontrada->nome), stdin);
+    fflush(stdin);
+
+    printf("\nDigite a idade: ");
+    scanf("%d", &encontrada->idade);
+}
+
+Pessoa* excluir(Pessoa *lista, int idBusca){
+    Pessoa *anterior = NULL;
+    Pessoa *atual = lista;
+
+    while(atual != NULL && atual->id != idBusca){
+        anterior = atual;
+        atual = atual->prox;
+    }
+    if(atual != NULL){
+        if(anterior != NULL){  //excluir oq nao e o primeiro
+            anterior->prox = atual->prox;
+
+        } else {  //excluir o primeiro da lista
+            lista = atual->prox;
+        }
+        free(atual);
+        printf("Excluido com sucesso");
+        printf("\n");
+    } else {
+        printf("\nPessoa nao encontrada");
+    }
+    return lista;
+}
+
+void liberarLista(Pessoa *lista){
+    Pessoa *atual = lista;
+    Pessoa *proximaPessoa;
+
+    while(atual != NULL){
+        proximaPessoa = atual->prox;
+        free(atual);
+        atual = proximaPessoa;
+    }
+}
+
 main(){
 
-    int opcao;
+    int opcao, idBusca;
     Pessoa *lista = criarListaVazia();
+    Pessoa *encontrada;
 
     do{
         printf("\nDigite 1 para cadastrar.");
@@ -71,15 +159,28 @@ main(){
             break;
 
             case 2:
+                mostrar(lista);
             break;
 
             case 3:
+                printf("\nDigite o ID para busca: ");
+                scanf("%d", &idBusca);
+                encontrada = buscarPessoa(lista, idBusca);
             break;
 
             case 4:
+                printf("\nDigite o ID para alteracao: ");
+                scanf("%d", &idBusca);
+                encontrada = buscarPessoa(lista, idBusca);
+                if(encontrada != NULL){
+                    alterar(encontrada);
+                }
             break;
 
             case 5:
+                printf("\nDigite o ID para excluir: ");
+                scanf("%d", &idBusca);
+                lista = excluir(lista, idBusca);
             break;
 
             case 0:
@@ -87,4 +188,5 @@ main(){
         }
     }while(opcao != 0);
 
+    liberarLista(lista);
 }
